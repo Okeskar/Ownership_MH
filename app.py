91b397eaf3ed77efcdd5200ebe8a7aeac5fc2052
Onkar Keskar <onkar.keskar99@gmail.com>
2025-04-20 02:26:40 +0530

    Fixed deprecated app.run_server to app.run

--- app.py
+++ app.py
@@ -113,4 +113,4 @@ import os
 port = int(os.environ.get('PORT', 8050))
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=port, host='0.0.0.0')
+    app.run(debug=True, port=port, host='0.0.0.0')
